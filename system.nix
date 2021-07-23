343a3fe06011d4ab04bd8c85a1dfd67022eb2089
Felix Rath <felixm.rath@gmail.com>
2021-07-23 17:14:07 +0200

    system: switch to pipewire, solved some issues
    * Volume jumped to 100% sometimes, solved with PW
    * Changing volume would not be instant, but would have a delay; solved
    with PW

--- system.nix
+++ system.nix
@@ -109,12 +109,15 @@
   # services.printing.enable = true;
 
   # Enable sound.
+  # Mostly from https://discourse.nixos.org/t/headphone-volume-resets-itself-to-100/13866/2
   sound.enable = true;
-  hardware.pulseaudio = {
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-
-    # from https://nixos.wiki/wiki/PulseAudio
-    package = pkgs.pulseaudioFull;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
