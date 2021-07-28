030934cac61ffaa5f296d48bb66f99ec6dab83f4
Felix Rath <felixm.rath@gmail.com>
2021-07-28 13:58:23 +0000

    doom-emacs: add mode for meson.build files

--- dotfiles/doom-emacs/packages.el
+++ dotfiles/doom-emacs/packages.el
@@ -9,6 +9,8 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
+(package! meson-mode)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
