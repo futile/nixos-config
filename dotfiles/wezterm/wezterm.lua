d084c5a68f01ddb30cb2d41815bc82f8a02faaa2
Felix Rath <felixm.rath@gmail.com>
2023-06-22 17:20:15 +0200

    wezterm: change theme

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -1,27 +1,26 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 return {
-  -- color_scheme = 'nord',
-  -- color_scheme = 'Nord (base16)',
-  -- color_scheme = 'Nova (base16)',
-  -- color_scheme = 'nordfox',
-  -- color_scheme = 'Seafoam Pastel',
-  -- color_scheme = 'Solarized (light) (terminal.sexy)',
-  -- color_scheme = 'kanagawabones',
-  -- color_scheme = 'onedark',
-  color_scheme = 'everforest',
+	-- color_scheme = "nord",
+	-- color_scheme = "Nord (base16)",
+	-- color_scheme = "Nova (base16)",
+	-- color_scheme = "nordfox",
+	-- color_scheme = "Seafoam Pastel",
+	-- color_scheme = "Solarized (light) (terminal.sexy)",
+	color_scheme = "kanagawabones",
+	-- color_scheme = "onedark",
+	-- color_scheme = 'everforest',
 
-  font = wezterm.font_with_fallback {
-    'JetBrainsMono Nerd Font',
-    'Symbols Nerd Font Mono',
-    'Symbols Nerd Font',
-    'FiraCode Nerd Font Mono',
-    'FiraCode Nerd Font',
-  },
+	font = wezterm.font_with_fallback({
+		"JetBrainsMono Nerd Font",
+		"Symbols Nerd Font Mono",
+		"Symbols Nerd Font",
+		"FiraCode Nerd Font Mono",
+		"FiraCode Nerd Font",
+	}),
 
-  hide_tab_bar_if_only_one_tab = true,
+	hide_tab_bar_if_only_one_tab = true,
 
-  colors = {
-    tab_bar = {
-    },
-  },
+	colors = {
+		tab_bar = {},
+	},
 }
