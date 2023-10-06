290b62bd9b949c77baffc5fa4c4f34280acd804e
Felix Rath <felixm.rath@gmail.com>
2023-10-06 13:01:10 +0200

    nixos-work: enable all firmware

--- hosts/nixos-work/hardware-configuration.nix
+++ hosts/nixos-work/hardware-configuration.nix
@@ -45,5 +45,8 @@
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  hardware.enableAllFirmware = true;
+
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
