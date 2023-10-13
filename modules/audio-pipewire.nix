944768f9f8a603e173cb749c0f827b35f08f012c
Felix Rath <felixm.rath@gmail.com>
2023-10-13 18:06:41 +0700

    audio: some WH-XM-headset stuff

--- modules/audio-pipewire.nix
+++ modules/audio-pipewire.nix
@@ -10,4 +10,18 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  # from https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
+  # environment.etc = {
+  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #     bluez_monitor.properties = {
+  #       ["bluez5.enable-sbc-xq"] = true,
+  #       ["bluez5.enable-msbc"] = true,
+  #       ["bluez5.enable-hw-volume"] = true,
+  #     }
+  #   '';
+  # };
+
+  # not sure if this prevents hsp from working
+  # ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
 }
