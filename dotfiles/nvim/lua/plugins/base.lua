5987e8687d0e9bf71858ae5d23aa18b688eb83f5
Felix Rath <felixm.rath@gmail.com>
2023-11-01 18:39:56 +0700

    nvim-lazy: add tabout

--- dotfiles/nvim/lua/plugins/base.lua
+++ dotfiles/nvim/lua/plugins/base.lua
@@ -1,6 +1,63 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
 if true then return {
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  -- tabout.nvim
+  {
+    'abecodes/tabout.nvim',
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    -- opts = {},
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      local base_table = {}
+      if (opts['mapping'] ~= nil) then
+        base_table = opts.mapping
+      end
+
+      opts.mapping = vim.tbl_extend("force", base_table, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+
   -- codeium config, to support the downloaded LSP
   {
     "Exafunction/codeium.nvim",
