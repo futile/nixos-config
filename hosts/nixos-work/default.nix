576d3928a8084b9f064716a050c2670de53fb501
Felix Rath <felixm.rath@gmail.com>
2023-11-01 18:40:49 +0700

    docker: enable host-access from inside docker in iptables :upside_down_face:

--- hosts/nixos-work/default.nix
+++ hosts/nixos-work/default.nix
@@ -207,7 +207,7 @@ in {
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedTCPPorts = [ 8080 ]; # this worked :)
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
--- modules/docker.nix
+++ modules/docker.nix
@@ -7,4 +7,11 @@
       fixed-cidr-v6 = "fd00::/80";
     };
   };
+
+  # Allow access from inside docker containers to the host (usually using host.docker.internal/172.17.0.1)
+  # based on https://stackoverflow.com/a/52560944
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -i br+ -j ACCEPT
+    iptables -A INPUT -i docker0 -j ACCEPT
+  '';
 }
