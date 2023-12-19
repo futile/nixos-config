b22079c125ef297258c8c3c31a94d52a37de7f1c
Felix Rath <felixm.rath@gmail.com>
2023-12-19 17:02:46 +0100

    slack: screensharing fix

--- hosts/nixos-work/home.nix
+++ hosts/nixos-work/home.nix
@@ -63,7 +63,26 @@ in {
         signal-desktop
         tdesktop
         discord
-        slack
+        # slack
+        # fix for screen sharing (hopefully), from https://discourse.nixos.org/t/slack-screensharing-gnome-wayland/35585/8
+        (slack.overrideAttrs (oldAttrs: rec {
+          # version = "4.35.126";
+          # src = pkgs.fetchurl {
+          #   url =
+          #     "https://downloads.slack-edge.com/releases/linux/${version}/prod/x64/slack-desktop-${version}-amd64.deb";
+          #   sha256 = "sha256-ldFASntF8ygu657WXwk/XlpHzB+S9x8SjAOjjDKsvCs=";
+          # };
+
+          fixupPhase = ''
+            sed -i -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
+
+            rm $out/bin/slack
+            makeWrapper $out/lib/slack/slack $out/bin/slack \
+              --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+              --suffix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+              --add-flags "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations,WebRTCPipeWireCapturer"
+          '';
+        }))
         # element-desktop # known bug: https://github.com/NixOS/nixpkgs/issues/120228
 
         # rust tools
