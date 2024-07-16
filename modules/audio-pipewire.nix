23e80934b3073066c56501125bc9939e153f4a92
Felix Rath <felixm.rath@gmail.com>
2024-07-16 22:39:05 +0200

    flake.lock: Update to fix nixos-home build, also remove `sound.enable`
    `sound.enable` was removed upstream, also didn't do anything useful
    anyway it seems.

--- modules/audio-pipewire.nix
+++ modules/audio-pipewire.nix
@@ -1,7 +1,6 @@
 { ... }: {
   # Set-up pipewire for audio.
   # Mostly from https://discourse.nixos.org/t/headphone-volume-resets-itself-to-100/13866/2
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
