3be1e0ffa8505017a8d42389b02f8b2041821059
Felix Rath <felixm.rath@gmail.com>
2025-12-14 20:29:46 +0100

    niri: tabbing conf & swayidle conf

--- home-modules/niri.nix
+++ home-modules/niri.nix
@@ -29,6 +29,27 @@
     };
   };
 
+  # based on  https://yalter.github.io/niri/Example-systemd-Setup.html
+  services.swayidle = {
+    enable = true;
+
+    timeouts = [
+      {
+        timeout = 600;
+        command = "${pkgs.swaylock}/bin/swaylock --daemonize --show-failed-attempts --show-keyboard-layout";
+      }
+      {
+        timeout = 601;
+        command = "${pkgs.niri}/bin/niri msg action power-off-monitors";
+      }
+    ];
+
+    events = {
+      "before-sleep" =
+        "${pkgs.swaylock}/bin/swaylock --daemonize --show-failed-attempts --show-keyboard-layout";
+    };
+  };
+
   # somewhat buggy with non-hyprland DE, i.e., gnome
   # home.packages = [ pkgs.wl-clipboard ];
 }
