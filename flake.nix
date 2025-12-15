ef6094ce577c5c73759e98cb141d981502b92539
Felix Rath <frath@cloudflare.com>
2025-12-15 12:42:52 +0100

    hm-cf: Workaround for spotify hash mismatch

--- flake.nix
+++ flake.nix
@@ -312,6 +312,20 @@
               };
             })
 
+            (final: prev: {
+              # fix for spotify hash mismatch, see https://github.com/NixOS/nixpkgs/issues/465676#issuecomment-3591991243
+              spotify = prev.spotify.overrideAttrs (oldAttrs: {
+                src =
+                  if (prev.stdenv.isDarwin && prev.stdenv.isAarch64) then
+                    prev.fetchurl {
+                      url = "https://web.archive.org/web/20251029235406/https://download.scdn.co/SpotifyARM64.dmg";
+                      hash = "sha256-0gwoptqLBJBM0qJQ+dGAZdCD6WXzDJEs0BfOxz7f2nQ=";
+                    }
+                  else
+                    oldAttrs.src;
+              });
+            })
+
             # inputs.cf-engineering-nixpkgs.overlay
           ];
         };
