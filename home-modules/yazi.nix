96e51b876f3ada2f6ec1379db66b45928585e0b7
Felix Rath <felixm.rath@gmail.com>
2026-03-09 00:33:18 +0100

    flake.lock,yazi: Update all inputs. Silence yazi notice.

--- home-modules/yazi.nix
+++ home-modules/yazi.nix
@@ -6,5 +6,9 @@
     enableBashIntegration = true;
     enableFishIntegration = true;
     enableNushellIntegration = true;
+
+    # 2026-03-08 modern version is "y", but I don't use this a lot anyway,
+    # so keep the old behavior to silence the warning.
+    shellWrapperName = "yy";
   };
 }
