fa998c14641d7dec76ea442c2a975b1625e05876
Felix Rath <felixm.rath@gmail.com>
2026-04-12 16:30:02 +0200

    codex: Symlink `agents/` folder

--- home-modules/codex.nix
+++ home-modules/codex.nix
@@ -8,4 +8,6 @@
   # 2026-04-11 Codex adds "trusted project paths" to the file, which are machine-specific, so can't share the file :/
   # home.file.".codex/config.toml".source =
   #   config.lib.file.mkOutOfStoreSymlink "${thisFlakePath}/dotfiles/codex/config.toml";
+  home.file.".codex/agents".source =
+    config.lib.file.mkOutOfStoreSymlink "${thisFlakePath}/dotfiles/codex/agents";
 }
