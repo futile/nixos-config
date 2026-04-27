e0e7339b6f9d5dffbc6dbcd20d81605edbb80ae6
Felix Rath <felixm.rath@gmail.com>
2026-04-27 16:02:53 +0200

    flake.nix: Add cachix source for `noctalia`

---
+++ cachix/noctalia.nix
@@ -0,0 +1,12 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://noctalia.cachix.org"
+      ];
+      trusted-public-keys = [
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      ];
+    };
+  };
+}
