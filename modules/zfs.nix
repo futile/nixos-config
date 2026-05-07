00cb0a8011cffa715458da7c9a52f5f835a2cf26
Felix Rath <felixm.rath@gmail.com>
2026-05-08 01:06:16 +0200

    flake.lock: Update all inputs

--- modules/zfs.nix
+++ modules/zfs.nix
@@ -8,6 +8,11 @@
 {
   boot.supportedFilesystems = [ "zfs" ];
 
+  # 2026-05-08 evaluation warning: `boot.zfs.forceImportRoot` is using the
+  # default value of `true`. It is highly recommended to set it to `false`, the
+  # new default from 26.11 on, to reduce the risk of data loss.
+  boot.zfs.forceImportRoot = false;
+
   # For a more recent ZFS version
   # boot.zfs.enableUnstable = true;
 
